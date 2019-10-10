e3894586ea45b59f6821d17fb0ee5dec4c8b6d6e
Andrew Matheny <andrew.j.matheny@gmail.com>
2019-10-10 13:46:21 -0400

    WIP

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -3,6 +3,13 @@ import * as functions from 'firebase-functions';
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-export const helloWorld = functions.https.onRequest((request, response) => {
- response.send("Hello from Firebase!");
+export const generateVoucherIntake = functions.https.onRequest((request, response) => {
+  console.log("Got request = ", { 
+    headers: request.rawHeaders,
+    body: JSON.stringify(request.body),
+  });
+  if (request.method !== "POST" || request.header("content-type") !== "application/json") {
+    return response.sendStatus(404);
+  }
+  return response.status(200).send(request.body);
 });
